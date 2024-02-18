a74c2f51a1677d7e57b598ddb25c7238706155ee
neuvecom <neuvecom@gmail.com>
2024-02-19 08:44:40 +0900

    最初のコミット

---
+++ doc/vivliostyle.conig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Flutter読書メモ',
+  author: 'neuvecom <neuvecom@gmail.com>',
+  language: 'ja',
+  size: 'B5',
+  theme: '@vivliostyle/theme-techbook@^1.0.0',
+  entry: [
+    'READINGMEMO.md',
+  ],
+  workspaceDir: '.vivliostyle',
+}
