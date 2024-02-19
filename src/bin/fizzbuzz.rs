daa68824760034e11d9c007e55f12582f93eef31
neuvecom <neuvecom@gmail.com>
2024-02-19 09:47:56 +0900

    Match版追加

--- src/bin/fizzbuzz.rs
+++ src/bin/fizzbuzz.rs
@@ -3,6 +3,8 @@ fn main() {
   let n: usize = 20;
   println!("FizzBuzz");
   fizz_buzz(n);
+  println!("FizzBuzz Match ver.");
+  fizz_buzz_match(n);
 }
 
 fn fizz_buzz(n: usize) {
@@ -15,6 +17,17 @@ fn fizz_buzz(n: usize) {
       println!("{} : Buzz", i);
     } else {
       println!("{}", i);
+    }
+  }
+}
+
+fn fizz_buzz_match(n: usize) {
+  for i in 1..=n {
+    match(i % 3, i % 5) {
+      (0, 0) => println!("{} : FizzBuzz", i),
+      (0, _) => println!("{} : Fizz", i),
+      (_, 0) => println!("{} : Buzz", i),
+      _ => println!("{}", i),
     }
   }
 }
