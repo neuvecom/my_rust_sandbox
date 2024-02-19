d17cef9e111719b7a7149737a255ef6c2cb912e7
neuvecom <neuvecom@gmail.com>
2024-02-19 12:03:53 +0900

    曜日の日本語化

--- src/bin/zeller.rs
+++ src/bin/zeller.rs
@@ -1,10 +1,52 @@
+// 列挙型で曜日データの構成を行う
+enum DayOfWeek {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+
+// 数値から列挙型への変換を行う
+impl DayOfWeek {
+  fn from_iso_number(iso_number: i32) -> DayOfWeek {
+    match iso_number {
+      1 => DayOfWeek::Monday,
+      2 => DayOfWeek::Tuesday,
+      3 => DayOfWeek::Wednesday,
+      4 => DayOfWeek::Thursday,
+      5 => DayOfWeek::Friday,
+      6 => DayOfWeek::Saturday,
+      7 => DayOfWeek::Sunday,
+      _ => unreachable!(),
+    }
+  }
+
+  // 列挙型から曜日の文字列を取得
+  fn to_ja(&self) -> char {
+    match self {
+      DayOfWeek::Sunday => '日',
+      DayOfWeek::Monday => '月',
+      DayOfWeek::Tuesday => '火',
+      DayOfWeek::Wednesday => '水',
+      DayOfWeek::Thursday => '木',
+      DayOfWeek::Friday => '金',
+      DayOfWeek::Saturday => '土',
+    }
+  }
+}
+
 fn main() {
   let year: i32 = 2023;
   let month: i32 = 8;
   let day: i32 = 4;
 
   let zeller_iso: i32 = zeller(year, month, day);
-  println!("{:04}年{:02}月{:02}日({})", year, month, day, zeller_iso);
+  let day_of_week: DayOfWeek = DayOfWeek::from_iso_number(zeller_iso);
+  let day_of_week_ja: char = day_of_week.to_ja();
+  println!("{:04}年{:02}月{:02}日({})", year, month, day, day_of_week_ja);
 }
 
 fn zeller(y: i32, m: i32, d: i32) -> i32 {
